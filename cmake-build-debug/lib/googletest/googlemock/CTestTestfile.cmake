a406427690e7b52b0f2f4634ed96a2b8aea597b6
robbe <php.robbe.claessens@gmail.com>
2018-02-02 16:32:47 +0100

    Init

---
+++ cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robbe/CLionProjects/patterns/lib/googletest/googlemock
+# Build directory: /home/robbe/CLionProjects/patterns/cmake-build-debug/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ cmake-build-debug/lib/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robbe/CLionProjects/patterns/lib/googletest/googletest
+# Build directory: /home/robbe/CLionProjects/patterns/cmake-build-debug/lib/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
