a9ddb71732e1b614f59004b6fe8db032e1bc431b
robbe <php.robbe.claessens@gmail.com>
2018-02-02 16:36:03 +0100

    removed unimportant fiels

--- cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robbe/CLionProjects/patterns/lib/googletest
-# Build directory: /home/robbe/CLionProjects/patterns/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/lib/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robbe/CLionProjects/patterns/lib/googletest/googletest
-# Build directory: /home/robbe/CLionProjects/patterns/cmake-build-debug/lib/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
