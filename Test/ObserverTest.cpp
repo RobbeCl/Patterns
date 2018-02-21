45accbf8e0a99cebfa64d65f00ba53b0ca75dbbe
Joey De Pauw <joeydepauw@gmail.com>
2018-02-21 12:06:45 +0100

    lowered Cmake version, styling changes

--- Test/ObserverTest.cpp
+++ Test/ObserverTest.cpp
@@ -1,18 +1,3 @@
-/*
- *  This is free software: you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  any later version.
- *  The software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  You should have received a copy of the GNU General Public License
- *  along with the software. If not, see <http://www.gnu.org/licenses/>.
- *
- *  Copyright 2017, Willem L, Kuylen E, Stijven S & Broeckhove J
- */
-
 /**
  * @file
  * Implementation of unit tests for observer pattern.
@@ -24,8 +9,6 @@
 
 class Topic{
 public:
-	virtual ~Topic() = default;
-
 	virtual void func1(){};
 	virtual void func2(int a, const std::string& b)=0;
 };
@@ -40,8 +23,6 @@ class Observer: public Topic{
 public:
 	bool func1Called = false;
 	bool func2Called = false;
-
-	virtual ~Observer(){}
 
 	virtual void func1(){;
 		func1Called = true;
